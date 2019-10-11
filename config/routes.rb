7aa6b0c9085023fcc0e4c27305a919753ef2a969
smaeruf <smaeruf@comcast.net>
2019-10-11 02:20:51 +0000

    building lessons

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,11 @@ Rails.application.routes.draw do
   resources :courses, only: [:index, :show]
   get '/instructor/courses', to: 'instructor/courses#new'
   namespace :instructor do
-    resources :courses, only: [:new, :create, :show] do
-      resources :sections, only: [:new, :create]
+      resources :sections, only: [] do
+        resources :lessons, only: [:new, :create]
+      end
+      resources :courses, only: [:new, :create, :show] do
+        resources :sections, only: [:new, :create]
     end
   end
 end
