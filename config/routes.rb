1738eb231be3dbba2ff9d6ff30e3acc6d04f8912
smaeruf <smaeruf@comcast.net>
2019-12-12 19:43:56 +0000

    lesson drag and drop

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   get '/instructor/courses', to: 'instructor/courses#new'
   namespace :instructor do
       resources :sections, only: [] do
-        resources :lessons, only: [:new, :create]
+      resources :lessons, only: [:new, :create]
       end
       resources :courses, only: [:new, :create, :show] do
         resources :sections, only: [:new, :create]
